2df291514a51b37d987ff92b63a58fa37569238d
Daniel <danieldascola@live.se>
2020-03-31 14:27:06 +0200

    Med kommentarer

--- Lektion6/lambda.py
+++ Lektion6/lambda.py
@@ -1,22 +1,22 @@
-import operator
+import operator #importerar funktionen operator
 
-operatorlookup = {'+': operator.add, '-': operator.sub, '*': operator.mul, '/': operator.truediv, '%': operator.mod}
+operatorlookup = {'+': operator.add, '-': operator.sub, '*': operator.mul, '/': operator.truediv, '%': operator.mod} #omvandlar stängtecken till ett ett räknesätt
 
 def input_1():
-    number_1 = int(input("Enter first number: "))
-    number_2 = int(input("Enter second number: "))
-    arithmetic = input("Choose arithmetic (+, -, /, *, %): ")
-    op = operatorlookup.get(arithmetic)
-    list_a = [number_1, number_2, op]
-    return list_a
+    number_1 = int(input("Enter first number: ")) #frågar om nummer 1
+    number_2 = int(input("Enter second number: ")) #frågar om nummer 2
+    arithmetic = input("Choose arithmetic (+, -, /, *, %): ") #frågar om ett specifikt räknesätt
+    op = operatorlookup.get(arithmetic) #tar strängen från 'arithmetic' och omvandlar till räknesätt och sparar det i variabeln op
+    list_a = [number_1, number_2, op] #sparar number_1 number_2 och op i en lista
+    return list_a #returnerar list_a
 
-x = input_1()
+x = input_1() #sparar outputen från  input_1() i variabeln x
 
 
-def print_lambda(v1, v2, lambda_f):
-    print(f"{v1} och {v2} ger: {lambda_f(v1, v2)}")
+def print_lambda(v1, v2, lambda_f): #definierar funktion som tar 3 argument
+    print(f"{v1} och {v2} ger: {lambda_f(v1, v2)}") #resultatet av 'v1' och 'v2' blir 'lambda_f(v1, v2)'
 
-print_lambda(x[0], x[1], x[2])
+print_lambda(x[0], x[1], x[2]) #kallar på funktionen 'print_lambda' och skickar med argumenten som ligger i variabeln x
 
 
     
