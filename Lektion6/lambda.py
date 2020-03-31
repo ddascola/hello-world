f2953eae73fa525f2c2d8821300b9be91a7afab1
Daniel <danieldascola@live.se>
2020-03-31 14:05:36 +0200

    Lambda uppgift

---
+++ Lektion6/lambda.py
@@ -0,0 +1,22 @@
+import operator
+
+operatorlookup = {'+': operator.add, '-': operator.sub, '*': operator.mul, '/': operator.truediv}
+
+def input_1():
+    number_1 = int(input("Enter first number: "))
+    number_2 = int(input("Enter second number: "))
+    arithmetic = input("Choose arithmetic (+, -, /, *): ")
+    op = operatorlookup.get(arithmetic)
+    list_a = [number_1, number_2, op]
+    return list_a
+
+x = input_1()
+
+
+def print_lambda(v1, v2, lambda_f):
+    print(f"{v1} och {v2} ger: {lambda_f(v1, v2)}")
+
+print_lambda(x[0], x[1], x[2])
+
+
+    
