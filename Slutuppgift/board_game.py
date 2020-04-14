7ed7057fea46962307a94d482db6a89483ee5504
Daniel <danieldascola@live.se>
2020-04-14 09:08:42 +0200

    Ej klart

---
+++ Slutuppgift/board_game.py
@@ -0,0 +1,138 @@
+import pickle
+import json
+import numpy as np
+import pandas as pd
+import csv
+class Game:
+    def __init__ (self, title, players, duration, age):
+        self.title = title.title()
+        self.players = players
+        self.duration = duration
+        self.age = age
+    
+    def set_title(self, title):
+        self.title = title
+    
+    def set_players(self, players):
+        self.players = players
+    
+    def set_duration(self, duration):
+        self.duration = duration
+    
+    def set_age(self, age):
+        self.age = age
+
+    def __repr__(self):
+        return "Game title:% s players:% s duration:% s age:% s" % (self.title, self.players, self.duration, self.age)
+
+
+class Game_cabinet:
+    def __init__(self):
+        self.game_list = []
+
+
+    def add_game(self, title, players, duration, age):
+        temp_game = Game(title, int(players), int(duration), int(age))
+        self.game_list.append(temp_game)
+
+    def remove_game(self, title):
+        for i, game in enumerate(self.game_list):
+            if game.title == title:
+                del self.game_list[i]
+                break
+        
+            
+
+cabinet = Game_cabinet()
+#def game_menu(games):
+choice = 0
+while choice != 6:
+    choice = int(input("1.Add new game\n2.Show list of games\n3.Save your newly added games\n4.Show list of games from file\n5.Remove a game\nMake your choice: "))
+    if choice == 1:
+        print("Please enter title, recommended amount of players, duration in minutes and minimum age for the game")
+        title = input("Title: ")
+        #title = "monopol"
+        players = int(input("Recommended amount of players: "))
+        #players = int(4)
+        duration = int(input("Duration of game in minutes: "))
+        #duration = int(75)
+        age = int(input("Minimum age for the game: "))
+        #age = int(13)
+
+        cabinet.add_game(title, players, duration, age)
+        
+        
+    elif choice == 2:
+        for game in cabinet.game_list:
+            print(f"Title: {game.title}, Players: {game.players}, Duration: {game.duration}, Age: {game.age}")
+
+    elif choice == 3:
+        with open('boardgame.csv', 'a', newline = "") as list_of_games:
+            wr = csv.writer(list_of_games, quoting = csv.QUOTE_NONE)
+            wr.writerow(cabinet.game_list)
+
+    elif choice == 4:
+        with open('boardgame.csv', newline= "") as list_of_games:
+            complete_list = list(csv.reader(list_of_games))
+            game_array = np.array(complete_list)
+            #dataset = pd.DataFrame({"Title": game_array[:, 0], "Players": game_array[:,1], "Duration": game_array[:, 2], "Age": game_array[:, 3]})
+            #print(game_array)
+            #print(*complete_list, sep="\n")
+            print(complete_list)
+
+    elif choice == 5:
+        with open('boardgame.csv', newline= "") as list_of_games:
+            complete_list = list(csv.reader(list_of_games))
+            cabinet.game_list.extend(complete_list)
+            title = str(input("Which game would you like to delete? "))
+            title = title.title()
+            cabinet.game_list.remove(title)
+        with open('boardgame.csv', 'w', newline = "") as list_of_games:
+            wr = csv.writer(list_of_games, quoting = csv.QUOTE_NONE)
+            wr.writerow(cabinet.game_list)
+            
+
+
+#print(cabinet.game_list)
+#write = (f"Title: {game.title}, Players: {game.players}, Duration: {game.duration}, Age: {game.age}")
+#print(write)
+
+
+
+#my_cabinet = Game_cabinet()
+#game_menu(my_cabinet)
+
+
+
+
+
+
+
+
+
+
+
+'''storage = Cabinet()
+storage.add_game("Monopol", int(5), int(75), int(12))
+game_1 = Game("Ticket", 6, 75, 12)
+game_2 = Game("King of Tokyo", 4, 30, 12)
+game_3 = Game("Catan", 5, 60, 13)
+list_a = []
+list_a.append(game_1)
+list_a.append(game_2)
+list_a.append(game_3)
+#print([new_game])
+#print(storage.game_list)
+print(list_a)'''
+
+
+
+
+
+
+
+
+
+
+    
+
