0120dace3992b55735305b7c18d51dac0775eee0
unknown <danieldascola@live.se>
2020-04-22 19:53:19 +0200

    Slutgiltig version

--- Slutuppgift/board_game.py
+++ Slutuppgift/board_game.py
@@ -9,13 +9,13 @@ class Game(dict):
         self['max_duration'] = max_duration
         self['age'] = age
 
-class Game_cabinet(dict):
+class Game_cabinet(dict): #9
     
     def add_game(self, title, min_players, max_players, min_duration, max_duration, age):
         temp_game = Game(title, int(min_players), int(max_players),int(min_duration), int(max_duration), int(age))
         self[title] = temp_game
 
-def user_input(message):
+def user_input(message): #10, #11. Here I wanted to make a function to be sure that the value entered by the user is an integer.
     while True:
         try:
             global input_value
@@ -46,14 +46,14 @@ def add_a_game():
 
     cabinet.add_game(title, min_players, max_players, min_duration, max_duration, age)
 
-def show_games():
+def show_games(): #4, #12. This function is made to display all the saved games to the user in an easy to read dataframe.
     with open('boardgame.json', 'r') as list_of_games:
         boardgame_dict = json.load(list_of_games)
         boardgame_df = pd.DataFrame(boardgame_dict)
-        boardgame_df = boardgame_df.T
+        boardgame_df = boardgame_df.T # This is used to change the position of the keys from axis 0 to axis 1 in the dataframe.
         print(boardgame_df)
 
-def change_game():
+def change_game(): #1, #2
     with open('boardgame.json', 'r') as list_of_games:
         boardgame_list = json.load(list_of_games)
         game_id = str(input('Which game woud you like to change? '))
@@ -66,21 +66,25 @@ def change_game():
     with open('boardgame.json', 'w') as list_of_games:
         json.dump(boardgame_list, list_of_games)
 
-def remove_game():
+def remove_game(): #3.
     with open('boardgame.json', 'r') as list_of_games:
         boardgame_dict = json.load(list_of_games)
         game_id = str(input('Which game do you want to remove? '))
         game_id = game_id.lower()
-        del boardgame_dict[game_id]
-    with open('boardgame.json', 'w') as list_of_games:
-        json.dump(boardgame_dict, list_of_games)
-        print(game_id.title(), 'was successfully removed')
+        try: # Here I wanted to make sure that the game a user tries to remove actually exists
+            del boardgame_dict[game_id]
+            with open('boardgame.json', 'w') as list_of_games:
+                json.dump(boardgame_dict, list_of_games)
+                print(game_id.title(), 'was successfully removed')
+        except KeyError:
+            print('The game does not exist')
 
-def filter_game():
+def filter_game(): #5, #6, #7, #8
     with open('boardgame.json', 'r') as list_of_games:
         boardgame_dict = json.load(list_of_games)
         boardgame_df = pd.DataFrame(boardgame_dict)
         boardgame_df = boardgame_df.T
+        # To be able to compare the value entered by the user and the values in the dataframe, there is a need to convert the dataframe values to numeric values.
         boardgame_df[['min_players', 'max_players', 'min_duration', 'max_duration', 'age']] = boardgame_df[['min_players', 'max_players', 'min_duration', 'max_duration', 'age']].apply(pd.to_numeric)
         
         user_input('How many players are you? ')
@@ -92,6 +96,7 @@ def filter_game():
         user_input('How old is the youngest player in your group? ')
         inp_age = input_value
         
+        # Declaring the evaluation order of the statements for filtering the dataframe with '()', and combining the statements with '&'.
         matches_df = boardgame_df[(boardgame_df.min_players <= inp_players) & (boardgame_df.max_players >= inp_players) & (boardgame_df.min_duration <= inp_duration) & (boardgame_df.max_duration >= inp_duration) & (boardgame_df.age <= inp_age)]
         if matches_df.empty == True:
             print('No games matching the criteria')
@@ -107,7 +112,7 @@ def save_game():
 
 cabinet = Game_cabinet()
 choice = 0
-while choice != None:
+while choice != None: # Making room for user mistakes while making their choice. If for example I were to have an 8 instead of None and the user entered an 8 by mistake, the program would stop.
     user_input('1.Add new game\n2.Save your newly added games\n3.Show list of games from file\n4.Make changes to a game\n5.Remove a game\n6.Search for a suitable game\n7.Exit program\nMake your choice: ')
     choice = input_value
     if choice == 1:
@@ -131,8 +136,22 @@ while choice != None:
         filter_game()
 
     elif choice == 7:
-        save_game()
+        save_game() # Safety feature if the user forgets to save their games and exits the program.
         exit()
 
     else:
         print('Choose an alternative between 1 and 7')
+
+#Sources:
+#1. https://stackoverflow.com/questions/12309269/how-do-i-write-json-data-to-a-file
+#2. https://www.geeksforgeeks.org/python-update-nested-dictionary/
+#3. https://www.programiz.com/python-programming/nested-dictionary Example: 6
+#4. https://www.youtube.com/watch?v=x3ueeuoU2x0
+#5. https://www.youtube.com/watch?v=2AFGPdNn4FM
+#6. https://stackoverflow.com/questions/15891038/change-data-type-of-columns-in-pandas
+#7. https://www.youtube.com/watch?v=YPItfQ87qjM
+#8. https://pythonexamples.org/pandas-check-if-dataframe-is-empty/
+#9. https://www.quora.com/How-do-I-create-a-constructor-without-arguments-in-python
+#10. https://stackoverflow.com/questions/23294658/asking-the-user-for-input-until-they-give-a-valid-response
+#11. https://stackoverflow.com/questions/50559252/python-while-true-try-except-returning-value
+#12. https://stackoverflow.com/questions/31658183/how-to-switch-columns-rows-in-a-pandas-dataframe
